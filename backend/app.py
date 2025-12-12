decf142d05edef15b07dfb480c3a23445db3695d
Dishant <dishant03maithani@gmail.com>
2025-12-12 22:50:35 +0530

    Update backend and add missing files

--- backend/app.py
+++ backend/app.py
@@ -17,4 +17,5 @@ def predict():
     return jsonify({"sentiment": pred})
 
 if __name__ == "__main__":
-    app.run(debug = True, port = 8000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
